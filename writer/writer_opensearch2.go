2b2c55549819986dfa86009a36eb98243f7c0f0d
sfomuseumbot <sfomuseumbot@localhost>
2023-10-03 15:46:34 -0700

    add ClientOptionsFromURI, NewClientFromOptions; change NewClient method signature

--- writer/writer_opensearch2.go
+++ writer/writer_opensearch2.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/url"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -56,73 +55,28 @@ type OpensearchV2Writer struct {
 // * ?workers={INT}. The number of users to enable for bulk indexing. Default is 10.
 func NewOpensearchV2Writer(ctx context.Context, uri string) (wof_writer.Writer, error) {
 
+	os_client_opts, err := wof_client.ClientOptionsFromURI(ctx, uri)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create client options, %w", err)
+	}
+
+	os_client, err := wof_client.NewClientFromOptions(ctx, os_client_opts)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create ES client, %w", err)
+	}
+
+	opensearch_index := os_client_opts.Index
+	debug := os_client_opts.Debug
+
 	u, err := url.Parse(uri)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to parse URI, %w", err)
 	}
 
-	var opensearch_endpoint string
-
-	port := u.Port()
-
-	// TO DO: update to support multiple addresses and/or the fact that TLS may be
-	// enabled on a non-443 port
-
-	switch port {
-	case "443":
-		opensearch_endpoint = fmt.Sprintf("https://%s", u.Host)
-	default:
-		opensearch_endpoint = fmt.Sprintf("http://%s", u.Host)
-	}
-
-	opensearch_index := strings.TrimLeft(u.Path, "/")
-
-	debug := false
-
 	q := u.Query()
-
-	q_debug := q.Get("debug")
-	q_insecure := q.Get("insecure")
-	q_username := q.Get("username")
-	q_password := q.Get("password") // update to use go-runtime
-	q_aws_credentials_uri := q.Get("aws-credentials-uri")
-
-	os_client_opts := &wof_client.ClientOptions{
-		Addresses:         []string{opensearch_endpoint},
-		Username:          q_username,
-		Password:          q_password,
-		AWSCredentialsURI: q_aws_credentials_uri,
-	}
-
-	if q_debug != "" {
-
-		v, err := strconv.ParseBool(q_debug)
-
-		if err != nil {
-			return nil, fmt.Errorf("Failed to parse ?debug= parameter, %w", err)
-		}
-
-		debug = v
-		os_client_opts.Debug = debug
-	}
-
-	if q_insecure != "" {
-
-		insecure, err := strconv.ParseBool(q_insecure)
-
-		if err != nil {
-			return nil, fmt.Errorf("Failed to parse ?insecure= parameter, %w", err)
-		}
-
-		os_client_opts.Insecure = insecure
-	}
-
-	os_client, err := wof_client.NewClient(ctx, os_client_opts)
-
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create ES client, %w", err)
-	}
 
 	logger := log.New(io.Discard, "", 0)
 
@@ -178,7 +132,7 @@ func NewOpensearchV2Writer(ctx context.Context, uri string) (wof_writer.Writer, 
 			},
 			// OnFlushStart func(context.Context) context.Context // Called when the flush starts.
 			OnFlushEnd: func(context.Context) {
-				if wr.debug {				
+				if wr.debug {
 					wr.logger.Printf("[opensearch][debug] bulk indexer flush end")
 				}
 			},
